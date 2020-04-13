1b61be9f6898385a9b4b5ca008c855c77b328701
tc-uidev <56131568+tc-uidev@users.noreply.github.com>
2020-04-13 18:45:38 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
